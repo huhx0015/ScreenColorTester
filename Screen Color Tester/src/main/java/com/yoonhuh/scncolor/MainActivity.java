d41567ed03f07cdb8e67218055b6d175d05a06a5
Michael Y. Huh <mhuh@yoonhuh.com>
2014-03-13 16:49:12 -0700

    3/13/2014 (4:47 PM PST):
    * Cleaned up code. Removed functions that were useless.
    * UI overhaul. Now much more user friendly.
    * Removed additional files that were useless.
    * Added new logo and icon file.

--- Screen Color Tester/src/main/java/com/yoonhuh/scncolor/MainActivity.java
+++ Screen Color Tester/src/main/java/com/yoonhuh/scncolor/MainActivity.java
@@ -1,45 +1,61 @@
 package com.yoonhuh.scncolor;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-
+import android.widget.FrameLayout;
 import java.util.Random;
 
-/* yH-Screen-Color-Tester
- * PROGRAMMER: Michael Yoon Huh (huhx0015)
- * LAST UPDATED: 9/7/2013
+/** -----------------------------------------------------------------------------------------------
+ *  [MainActivity] CLASS
+ *  PROGRAMMER: Michael Yoon Huh (HUHX0015)
+ *  DESCRIPTION: MainActivity class is responsible for handling the first activity in the app and sets
+ *  up the references and the click actions for the color buttons.
+ *  -----------------------------------------------------------------------------------------------
  */
 
 public class MainActivity extends Activity {
 
+    /** CLASS VARIABLES ________________________________________________________________________ **/
+
     // Button variables.
-    Button red, black, white, green, grey, brown, pink, blue, teal, purple, random;
+    Button red, black, white, green, grey, brown, pink, blue, purple, random;
 
-    // Intent variables.
-    Intent currentIntent;
+    /** ACTIVITY LIFECYCLE FUNCTIONALITY _______________________________________________________ **/
 
+    // onCreate(): The initial function that is called when the activity is run. onCreate() only runs
+    // when the activity is first started.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpColorButtons(); // Sets up the color buttons.
     }
 
-    @Override
-    // Function that is responsible for actions on current screen.
-    public boolean onCreateOptionsMenu(Menu menu) {
+    /** ADDITIONAL FUNCTIONALITY _______________________________________________________________ **/
+
+    // randomColor(): Generates a random number for the random color background.
+    private int randomColor() {
+
+        // Generates random number and sets the variable.
+        Random rand = new Random();
+        int random_number = rand.nextInt(255 - 1) + 1; // Establishes the range to 255.
+
+        return random_number;
+    }
+
+    // setUpColorButtons(): Sets up the button listeners for the color buttons.
+    private void setUpColorButtons() {
 
         // Defines the local variables for the layout.
-        final RelativeLayout layout = (RelativeLayout) findViewById(R.id.main_layout);
+        final FrameLayout layout = (FrameLayout) findViewById(R.id.main_layout);
         final WindowManager.LayoutParams layoutParams = this.getWindow().getAttributes();
 
-        // Initializes and sets the proper Button resources (from layout) to the Button variables.
+        // Initializes and sets the proper Button resources to the Button variables.
         red = (Button) findViewById(R.id.bRed);
         white = (Button) findViewById(R.id.bWhite);
         blue = (Button) findViewById(R.id.bBlue);
@@ -51,32 +67,14 @@ public class MainActivity extends Activity {
         grey = (Button) findViewById(R.id.bGrey);
         random = (Button) findViewById(R.id.bRandom);
 
-
         // Action for pressing on the red button.
         red.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-               layout.setBackgroundColor(Color.RED); // Sets the background red.
-               layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
-               getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
-                /*
-                try {
-                    // Sets the path to whatever cheese is set to.
-                    Class ourClass = Class.forName("com.yoonhuh.scncolor.colorActivity");
-                    Intent ourIntent = new Intent(MainActivity.this, ourClass);
-                    startActivity(ourIntent);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                */
-
-                // currentIntent = new Intent("com.yoonhuh.scncolor.colorActivity");
-               // startActivity(currentIntent);
-               // PSUEDO CODE
-               // GO TO DIFFERENT SCREEN AND SHOW COLOR.
-               //
+                layout.setBackgroundColor(Color.RED); // Sets the background red.
+                layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
+                getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
             }
         });
 
@@ -88,7 +86,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.WHITE); // Sets the background white.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -100,7 +97,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.BLUE); // Sets the background blue.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -112,7 +108,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.GREEN); // Sets the background green.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -124,7 +119,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.rgb(255, 0, 255)); // Sets the background pink.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -147,7 +141,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.rgb(75, 8, 138)); // Sets the background purple.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -159,7 +152,6 @@ public class MainActivity extends Activity {
                 layout.setBackgroundColor(Color.BLACK); // Sets the background black.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
-
             }
         });
 
@@ -168,7 +160,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                layout.setBackgroundColor(Color.rgb(138, 138, 138)); // Sets the background black.
+                layout.setBackgroundColor(Color.rgb(138, 138, 138)); // Sets the background grey.
                 layoutParams.screenBrightness = 1.0F; // Raises the brightness level.
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
             }
@@ -184,20 +176,5 @@ public class MainActivity extends Activity {
                 getWindow().setAttributes(layoutParams); // Sets the attributes onto the layout.
             }
         });
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
-
-    // Generates a random number for the random color background.
-    public int randomColor() {
-
-        // Generates random number and sets the variable.
-        Random rand = new Random();
-        int random_number = rand.nextInt(255 - 1) + 1; // Establishes the range to 255.
-
-        return random_number;
-    }
-    
 }
